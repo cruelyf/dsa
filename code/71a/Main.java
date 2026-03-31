4e968a805045463803a9233aa54f8f072842b2b3
cruelyf <rahulturbo169@gmail.com>
2026-03-31 21:13:31 +0530

    Finised 71A

--- code/71a/Main.java
+++ code/71a/Main.java
@@ -1,18 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
-        
-        sc.nextLint();
+        sc.nextLine();
 
-        String[] strs = new String[w];
-
-        for ( int i = 0; i < w; i++ ) {
-            String word = strs[i]
+        for (int i = 0; i < w; i++) {
+            String word = sc.nextLine();
             int len = word.length();
-            if ( len > 10 ) {
-                System.out.println("" + word.charAt(0) + len + word.charAt(len - 1))
-            }
+
+            if (len > 10)
+                System.out.println("" + word.charAt(0) + (len - 2) + word.charAt(len - 1));
+            else
+                System.out.println(word);
         }
     }
 }
